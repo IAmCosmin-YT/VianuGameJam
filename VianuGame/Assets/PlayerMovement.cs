f4480a0d86bc965b89e3f6c351281501a05db437
ITO-Z <victorasoprescu@gmail.com>
2023-05-16 20:15:51 +0300

    Update
    Apesi shift sa tina minte

--- VianuGame/Assets/PlayerMovement.cs
+++ VianuGame/Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 mousePos;
     private int targetIndex = 0, posIndex = 0;
-    private bool canMove;
+    private bool canMove, singleClick;
     public Vector2[] posQueue = null;
     [SerializeField] float speed = 0.1f;
     [SerializeField] ParticleSystem particle;
@@ -20,23 +20,33 @@ public class PlayerMovement : MonoBehaviour
     {   
         if(Input.GetKeyDown(KeyCode.Mouse0)){
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if(posIndex != posQueue.Length){
-                canMove = true;
-                posQueue[posIndex++] = mousePos;
-                text.text = "Move Queue: x" + posIndex.ToString();
+            if(Input.GetKey(KeyCode.LeftShift) == false){
+                singleClick = true;
+            }
+            else{
+                singleClick = false;
+                if(posIndex != posQueue.Length){
+                    canMove = true;
+                    posQueue[posIndex++] = mousePos;
+                    text.text = "Move Queue: x" + posIndex.ToString();
+                }
             }
             Instantiate(particle, mousePos,Quaternion.identity);
         }
-        if(Input.GetKeyDown(KeyCode.Mouse1)){
-            canMove = false;    
-            ResetQueue();
-        }
+        //if(Input.GetKeyDown(KeyCode.Mouse1)){
+         //   canMove = false;    
+          //  ResetQueue();
+        //}
         if(posQueue[targetIndex] != Vector2.zero && canMove == true){
             MovePlayer(posQueue[targetIndex]);
             if(transform.position.x == posQueue[targetIndex].x && transform.position.y == posQueue[targetIndex].y) targetIndex++;
             if(targetIndex == posQueue.Length){
                 ResetQueue();
             }
+        }
+        if(singleClick){
+            ResetQueue();
+            MovePlayer(mousePos);
         }
 
     }
