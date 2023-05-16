8200694a14efd9f6b3848d5dc39e75d54bdcc4e9
ITO-Z <victorasoprescu@gmail.com>
2023-05-16 18:59:25 +0300

    Update PlayerMovement.cs
    GATA!!

--- VianuGame/Assets/PlayerMovement.cs
+++ VianuGame/Assets/PlayerMovement.cs
@@ -6,23 +6,27 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 mousePos;
     private int targetIndex = 0, posIndex = 0;
+    private bool canMove;
     [SerializeField] Vector2[] posQueue = null;
     [SerializeField] float speed = 0.1f;
     void Update()
     {   
         if(Input.GetKeyDown(KeyCode.Mouse0)){
+            canMove = true;
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if(posIndex != posQueue.Length){
                 posQueue[posIndex++] = mousePos;
             }
         }
-        if(posQueue[targetIndex] != Vector2.zero){
+        if(Input.GetKeyDown(KeyCode.Mouse1)){
+            canMove = false;    
+            ResetQueue();
+        }
+        if(posQueue[targetIndex] != Vector2.zero && canMove == true){
             MovePlayer(posQueue[targetIndex]);
             if(transform.position.x == posQueue[targetIndex].x && transform.position.y == posQueue[targetIndex].y) targetIndex++;
             if(targetIndex == posQueue.Length){
-                targetIndex = 0;
-                posIndex = 0;
-                posQueue = null;
+                ResetQueue();
             }
         }
 
@@ -32,6 +36,10 @@ public class PlayerMovement : MonoBehaviour
         Vector2 pos = transform.position;
         pos = Vector2.MoveTowards(transform.position, target, speed*Time.deltaTime);
         transform.position = pos;
-
+    }
+    void ResetQueue(){
+        targetIndex = 0;
+        posIndex = 0;
+        for(int i = 0; i<posQueue.Length; i++) posQueue[i] = Vector2.zero;
     }
 }
